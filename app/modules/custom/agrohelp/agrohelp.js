c314fa5a8cb80909c049701cf4b4ac86939e3016
ntym <mega_comp@mail.ru>
2018-07-27 12:25:32 +0300

    Мелкие правки

--- app/modules/custom/agrohelp/agrohelp.js
+++ app/modules/custom/agrohelp/agrohelp.js
@@ -32,6 +32,8 @@ function agrohelp_form_alter(form, form_state, form_id, aux)
         // console.log('agrohelp_form_alter - ');
         if (form_id === 'entityform_edit' && form.bundle === 'agrohelp') {
             var lang = language_default();
+            // изменить вывод формы запроса Агропомощи
+
             // todo извлечь поле с инструкциями из entityform type (drupalgap.content_types_list[bundle].data)
             var instruction = '<p>Если у Вас возникли затруднения с определением вредного объекта, будь то сорняк, вредитель или болезнь на Вашем участке или в поле, заполните форму ниже, чтобы наш специалист смог помочь.</p>'
                 + '<p>Профессиональный агроном проверит полученные данные и ответит по одному из оставленных контактов.</p>';
@@ -66,111 +68,4 @@ function agrohelp_form_alter(form, form_state, form_id, aux)
     catch (error) {
         console.log('agrohelp_form_alter - ' + error);
     }
-}
-
-
-
-function agrohelp_form(form, form_state)
-{
-    // todo сформировать форму автоматически по аналогии с node/%/edit
-
-    // используем автокомплит на Entity Index, так как нужны все значения словаря
-    form.elements['autocomplete_region'] = {
-        type: 'autocomplete',
-        // title: 'Регион',
-        remote: true,
-        custom: true,
-        handler: 'index',
-        entity_type: 'taxonomy_term',
-        vid: 29, /* optional vocabulary id filter */
-        value: 'tid',
-        label: 'name',
-        filter: 'name',
-        attributes: {
-            id: 'autocomplete-region',
-            'data-enhanced': true,
-            // 'data-inset': 'false',
-            'data-filter-placeholder': 'Регион'
-        },
-        prefix: '<h3>О себе</h3>'
-    };
-
-
-    // используем автокомплит с собственным обработчиком запросов,
-    // так как нужны не любые ноды, а типа main_cultures
-    // для этого на стороне сервера в модуле chibs будет функция chibs_autocomplete_cultures
-    form.elements['autocomplete_culture'] = {
-        type: 'autocomplete',
-        // title: 'Культура',
-        remote: true,
-        custom: true,
-        path: 'autocomplete-culture',
-        value: 'nid',
-        label: 'title',
-        filter: 'title',
-        params: 'limit=5',
-        attributes: {
-            id: 'autocomplete-culture',
-            'data-enhanced': true,
-            'data-filter-placeholder': 'Культура'
-        },
-        prefix: '<h3>Нужна помощь</h3>'
-    };
-
-    // используем автокомплит на Entity Index, так как нужны все значения словаря
-    form.elements['autocomplete_phase'] = {
-        type: 'autocomplete',
-        remote: true,
-        custom: true,
-        handler: 'index',
-        entity_type: 'taxonomy_term',
-        vid: 20, /* optional vocabulary id filter */
-        value: 'tid',
-        label: 'name',
-        filter: 'name',
-        attributes: {
-            id: 'autocomplete-phase',
-            'data-enhanced': true,
-            // 'data-inset': 'false',
-            'data-filter-placeholder': 'Фаза роста'
-        }
-    };
-
-    form.elements['image'] = {
-        type: 'image',
-        title: 'Фото',
-        required: true
-    };
-
-    // Buttons
-    form.elements['submit'] = {
-        type: 'submit',
-        value: 'Отправить'
-    };
-
-    return form;
-}
-
-function agrohelp_form_submit()
-{
-    // установка полей
-    var submission = {
-        type: 'contact_us',
-        field_contact_name: { und: { 0: { value: 'Барсук' }}},
-        field_phone:        { und: { 0: { value: '2-22-22' }}}
-    };
-
-    // отправка Запроса Агропомощи на сайт
-    agrohelp_entityform_submission_create({
-        data: JSON.stringify(submission),
-        success:function(result) {
-            alert('success');
-        },
-        error:function(xhr, status, message) {
-            try {
-                if (options.error) { options.error(xhr, status, message); }
-            }
-            catch (error) { console.log('agrohelp_entityform_submission_create - error - ' + error); }
-        }
-    });
 }
