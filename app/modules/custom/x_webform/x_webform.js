1843f50efb75e86e412da96db02b006e9e36fecd
ntym4ek <mega_comp@mail.ru>
2019-08-02 15:59:04 +0300

    правки

--- app/modules/custom/x_webform/x_webform.js
+++ app/modules/custom/x_webform/x_webform.js
@@ -3,16 +3,47 @@
  */
 
 /**
- * hook_form_alter()
+ * Implements hook_menu().
  */
-function x_webform_form_alter(form, form_state, form_id)
+function x_webform_menu()
 {
-    console.log('x_webform_form_alter - ');
-    try {
-        if (form_id === "webform_form") {
+    var items = {};
+    items['surveys'] = {
+        title: 'Опросы',
+        page_callback: 'x_webform_list_page'
+    };
 
-        }
-    } catch (error) { console.log('x_webform_form_alter - ' + error); }
+    return items;
+}
+
+/**
+ * ----------------------------------------------- Список опросов ------------------------------------------------------
+ */
+function x_webform_list_page()
+{
+    return {
+        'prefix': { markup: '<div class="row"><div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">' },
+        'list': {
+            theme: 'view',
+            path: 'surveys.json',
+            row_callback: 'x_webform_list_page_row'
+        },
+        'suffix': { markup: '</div></div>' }
+    };
+}
+
+function x_webform_list_page_row(view, row)
+{
+    try {
+        return theme('content_card', {
+            item: {
+                img: {src: row.img.src},
+                link: {url: 'node/' + row.nid},
+                title: row.title
+            }
+        });
+    }
+    catch (error) { console.log('x_webform_list_page_row - ' + error); }
 }
 
 /**
@@ -20,15 +51,21 @@ function x_webform_form_alter(form, form_state, form_id)
  */
 function x_webform_node_page_view_alter_webform(node, options)
 {
-    console.log('x_webform_node_page_view_alter_webform - ');
+    // console.log('x_webform_node_page_view_alter_webform - ');
     try {
-        drupalgap_set_title("Опрос");
+        delete(node.field_wf_category);
+        delete(node.field_promo_image);
+        delete(node.field_textfield_1);
+        drupalgap_entity_render_content('node', node);
         var content = {
-            'theme': 'node',
-            'node': node,
-            // @todo - this is a core field and should by fetched from entity.js
-            'title': { markup: "Опрос" },
-            'content': { markup: node.content }
+            'prefix': { markup: '<div class="row"><div class="col-xs-12 col-sm-10 col-sm-offset-1">' },
+            'suffix': { markup: '</div></div>' },
+            'header': '<div class="content-header"><h2>' + node.title + '</h2></div>'
+        };
+        content['questions'] = {
+            markup: '<div class="questions">' +
+                        node.content +
+                    '</div>'
         };
         options.success(content);
     }
